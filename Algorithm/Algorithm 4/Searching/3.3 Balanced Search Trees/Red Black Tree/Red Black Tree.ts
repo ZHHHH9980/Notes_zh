43d6f7c49ac341f7003d78a400696d2228e913ca
ZHHHH9980 <howzhongzh@gmail.com>
2022-10-17 08:39:23 +0800

    feat: add red black tree

--- Algorithm/Algorithm 4/Searching/3.3 Balanced Search Trees/Red Black Tree/Red Black Tree.ts
+++ Algorithm/Algorithm 4/Searching/3.3 Balanced Search Trees/Red Black Tree/Red Black Tree.ts
@@ -18,6 +18,12 @@ class RedBlackTreeNode<Key, Value> extends TreeNode<Key, Value> {
     return this.parent !== null && this.parent.right === this;
   }
 
+  public sibling() {
+    return this === this.parent?.left
+      ? (this.parent as RedBlackTreeNode<Key, Value>).right
+      : (this.parent as RedBlackTreeNode<Key, Value>).left;
+  }
+
   constructor(key, val, parent) {
     super(key, val);
     this.parent = parent;
@@ -36,23 +42,86 @@ class RedBlackTree<Key, Value> extends BBST<Key, Value> {
     return this._size;
   }
 
-  private _isBlack(node: RedBlackTreeNode<Key, Value>) {
-    return node && node.color === BLACK;
+  private _isBlack(node: RedBlackTreeNode<Key, Value> | null) {
+    return node === null || node.color === BLACK;
   }
 
-  private _black(node: RedBlackTreeNode<Key, Value>) {
+  private _isRed(node: RedBlackTreeNode<Key, Value> | null) {
+    return node && node.color === RED;
+  }
+
+  private _black(node: RedBlackTreeNode<Key, Value> | null) {
     if (node) node.color = BLACK;
+    return node;
+  }
+
+  private _red(node: RedBlackTreeNode<Key, Value>) {
+    if (node) node.color = RED;
+    return node;
   }
 
   private _afterAddNode(node: RedBlackTreeNode<Key, Value>) {
     const parent = node.parent;
 
-    // 添加的是根节点
+    // 添加的是根节点(根节点一定是黑色）
     if (parent === null) {
       this._black(node);
       return;
     }
 
-    // if
+    // 父节点是黑色， 直接返回
+    if (this._isBlack(parent)) {
+      return;
+    }
+
+    // 接下来是父节点为红色节点的情况
+
+    // 叔叔节点
+    const uncle = parent.sibling();
+    // 祖父节点
+    const grandparent = parent.parent as RedBlackTreeNode<Key, Value>;
+
+    // 叔叔节点是红色
+    if (this._isRed(uncle)) {
+      this._black(parent);
+      this._black(uncle);
+      // 染红组父节点，
+      // 在B树中相当于向上添加的新节点
+      // 在红黑树中可能会产生双红相连的现象。
+      this._red(grandparent);
+      this._afterAddNode(grandparent);
+      return;
+    }
+
+    // 叔叔节点不是红色
+    if (parent.isLeftChild()) {
+      // L
+      if (node.isLeftChild()) {
+        // LL
+        this._black(parent);
+        this._red(grandparent);
+        this._rotateRight(grandparent);
+      } else {
+        // LR
+        this._red(grandparent);
+        this._black(node);
+        this._rotateLeft(parent);
+        this._rotateRight(grandparent);
+      }
+    } else {
+      // R
+      if (node.isLeftChild()) {
+        // RL
+        this._red(grandparent);
+        this._black(node);
+        this._rotateRight(parent);
+        this._rotateLeft(grandparent);
+      } else {
+        // RR
+        this._black(parent);
+        this._red(grandparent);
+        this._rotateLeft(grandparent);
+      }
+    }
   }
 }
